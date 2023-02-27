990688f4cf4f0ca05421f11e31d4aaeb65968744
MShmatok <91060161+MShmatok@users.noreply.github.com>
2023-02-27 20:28:23 +0200

    27-02-2023__20-28

---
+++ js/setHeight.js
@@ -0,0 +1,9 @@
+
+(function init100vh() {
+    function setHeight() {
+        var vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+    }
+    setHeight();
+    window.addEventListener('resize', setHeight);
+})();
