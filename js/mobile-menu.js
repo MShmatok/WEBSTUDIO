ed16d3e17dea044a102bf255e4f32429ed6d8b61
MShmatok <91060161+MShmatok@users.noreply.github.com>
2023-02-28 22:39:39 +0200

    28-02-2023 22-39

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -12,7 +12,9 @@
         const scrollLockMethod = !isMenuOpen
             ? 'disableBodyScroll'
             : 'enableBodyScroll';
+
         bodyScrollLock[scrollLockMethod](document.body);
+
     };
 
     openMenuBtn.addEventListener('click', toggleMenu);
